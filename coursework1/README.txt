b9321a9f8010956dc2e2fcbff28a9784d552fcbf
Vincent Zhao <vincentzhaorz@gmail.com>
2016-10-29 21:08:15 +0100

    updated coursework1

---
+++ coursework1/README.txt
@@ -0,0 +1,9 @@
+Only have to modify SolveMNIST_Gradient.m
+
+Contents of the remaining files:
+
+evaluate_gB.m: Evaluate the functions & gradients
+mnist.mat:     Training & testing data with respect to random Fourier features
+mnist_original.mat:
+               The original image data
+
---
+++ coursework1/SolveMNIST_Gradient.m
@@ -0,0 +1,94 @@
+function ReturnVal = SolveMNIST_Gradient(tol, num_iter, step_size, ...
+                                         lambda)
+% Build a classifier for recognising hand-written digits from images
+%
+% Ruth Misener, 01 Feb 2016
+%
+% INPUTS: tol:       Optimality tolerance; check if algorithm converged
+%         num_iter:  Maximum number of iterations
+%         step_size: Step size
+%         lambda:    Regularisation parameter
+
+% Initialise the training set --------------------------------------------
+load mnist.mat
+n   = 1000; % Input features
+m   = 1000; % Test cases
+dim =   10;
+
+% l-2 Regulariser
+norm_type = 2;
+
+% Initialise a starting point for the algorithm --------------------------
+beta_guess = zeros(1,n*dim);
+
+beta_eval  = evaluate_gB(beta_guess, X, y, n, m, dim, lambda, ...
+                         0, norm_type);
+beta_grad  = evaluate_gB(beta_guess, X, y, n, m, dim, lambda, ...
+                         1, norm_type);
+
+% Store beta guesses at each iteration
+beta_guess_iter(1,:) = beta_guess; 
+
+% Store the function value at each iteration
+fcn_val_iter(1)      = beta_eval;  
+
+fprintf('\niter=%d; Func Val=%f; FONC Residual=%f',...
+        0, beta_eval, norm(beta_grad));
+
+% Iterative algorithm begins ---------------------------------------------
+for i = 1:num_iter                        
+    
+    % Step for gradient descent ------------------------------------------
+    % *** Insert gradient descent code here ***
+    % ***                                   ***
+    
+    % Update with the new iteration --------------------------------------
+    beta_guess_iter(i+1,:) = beta_guess;
+    
+    beta_eval              = evaluate_gB(beta_guess, X, y, n, m, dim, ...
+                                         lambda, 0, norm_type);
+                                     
+    fcn_val_iter(i+1)      = beta_eval;
+    
+    beta_grad              = evaluate_gB(beta_guess, X, y, n, m, dim, ...
+                                         lambda, 1, norm_type);
+                         
+    % Check if it's time to terminate ------------------------------------
+
+    % Check the FONC?
+    % Store the norm of the gradient at each iteration
+    convgsd(i) = 1; % <-- Correct this!!
+    
+    % Check that the vector is changing from iteration to iteration?
+    % Stores length of the difference between the current beta and the 
+    % previous one at each iteration
+    lenXsd(i)  = 1; % <-- Correct this!!
+    
+    % Check that the objective is changing from iteration to iteration?
+    % Stores the absolute value of the difference between the current 
+    % function value and the previous one at each iteration
+    diffFsd(i) = 1; % <-- Correct this!!
+    
+    fprintf('\niter=%d; Func Val=%f; FONC Residual=%f; Sqr Diff=%f',...
+            i, beta_eval, convgsd(i), lenXsd(i));
+    
+    % Check the convergence criteria?
+    if (convgsd(i) <= tol)
+        fprintf('\nFirst-Order Optimality Condition met\n');
+        break; 
+    elseif (lenXsd(i) <= tol)
+        fprintf('\nExit: Design not changing\n');
+        break;
+    elseif (diffFsd(i) <= tol)
+        fprintf('\nExit: Objective not changing\n');
+        break;
+    elseif (i + 1 >= num_iter)
+        fprintf('\nExit: Done iterating\n');
+        break;
+    end
+    
+end
+
+ReturnVal = beta_guess;
+
+end
